da60c8805b798e61d76bf1e4b21cf261405c19a3
Win Wang <ewangtf@gmail.com>
2024-04-02 01:02:38 +0800

    add zk rc class

---
+++ winrpc-core/src/main/java/cn/winwang/winrpc/core/registry/ZkRegistryCenter.java
@@ -0,0 +1,38 @@
+package cn.winwang.winrpc.core.registry;
+
+import cn.winwang.winrpc.core.api.RegistryCenter;
+
+import java.util.List;
+
+/**
+ * Description for this class.
+ *
+ * @author winwang
+ * @date 2024/4/2 1:00
+ */
+public class ZkRegistryCenter implements RegistryCenter {
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void register(String service, String instance) {
+
+    }
+
+    @Override
+    public void unregister(String service, String instance) {
+
+    }
+
+    @Override
+    public List<String> fetchAll(String service) {
+        return null;
+    }
+}
