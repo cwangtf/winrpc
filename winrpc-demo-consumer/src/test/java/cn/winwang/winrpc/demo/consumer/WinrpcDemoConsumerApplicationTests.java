f23f9e0c7be07241c7aa22cb961f9e26affb4c2a
Win Wang <ewangtf@gmail.com>
2024-04-19 23:47:33 +0800

    update consumer application test case

--- winrpc-demo-consumer/src/test/java/cn/winwang/winrpc/demo/consumer/WinrpcDemoConsumerApplicationTests.java
+++ winrpc-demo-consumer/src/test/java/cn/winwang/winrpc/demo/consumer/WinrpcDemoConsumerApplicationTests.java
@@ -1,13 +1,30 @@
 package cn.winwang.winrpc.demo.consumer;
 
+import cn.winwang.winrpc.demo.provider.WinrpcDemoProviderApplication;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class WinrpcDemoConsumerApplicationTests {
 
+    static ApplicationContext context;
+    @BeforeAll
+    static void init() {
+        context = SpringApplication.run(WinrpcDemoProviderApplication.class, "--server.port=8084", "--logging.level.cn.winwang.winrpc=debug");
+    }
+
     @Test
     void contextLoads() {
+        System.out.println(" ===> aaaa .... ");
+    }
+
+    @AfterAll
+    static void destroy() {
+        SpringApplication.exit(context, () -> 1);
     }
 
 }
