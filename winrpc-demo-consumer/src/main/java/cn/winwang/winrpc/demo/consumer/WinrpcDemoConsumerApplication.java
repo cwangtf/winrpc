8515dd9196f9fa401db7545600da90dd68db526a
Win Wang <ewangtf@gmail.com>
2024-04-21 00:52:41 +0800

    polish codes

--- winrpc-demo-consumer/src/main/java/cn/winwang/winrpc/demo/consumer/WinrpcDemoConsumerApplication.java
+++ winrpc-demo-consumer/src/main/java/cn/winwang/winrpc/demo/consumer/WinrpcDemoConsumerApplication.java
@@ -1,20 +1,14 @@
 package cn.winwang.winrpc.demo.consumer;
 
 import cn.winwang.winrpc.core.annotation.WinConsumer;
-import cn.winwang.winrpc.core.api.RpcRequest;
-import cn.winwang.winrpc.core.api.RpcResponse;
 import cn.winwang.winrpc.core.consumer.ConsumerConfig;
-import cn.winwang.winrpc.demo.api.Order;
-import cn.winwang.winrpc.demo.api.OrderService;
 import cn.winwang.winrpc.demo.api.User;
 import cn.winwang.winrpc.demo.api.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
