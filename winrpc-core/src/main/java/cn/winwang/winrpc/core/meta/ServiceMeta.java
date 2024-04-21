0774de2a14b7d8b93ea14ad555c3e14f8b9e95ca
Win Wang <ewangtf@gmail.com>
2024-04-21 11:53:51 +0800

    add service metas

--- winrpc-core/src/main/java/cn/winwang/winrpc/core/meta/ServiceMeta.java
+++ winrpc-core/src/main/java/cn/winwang/winrpc/core/meta/ServiceMeta.java
@@ -1,7 +1,11 @@
 package cn.winwang.winrpc.core.meta;
 
+import com.alibaba.fastjson.JSON;
 import lombok.Builder;
 import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 描述服务元数据.
@@ -21,7 +25,13 @@ public class ServiceMeta {
 
     private String name;
 
+    private Map<String, String> parameters = new HashMap<>();
+
     public String toPath() {
         return String.format("%s_%s_%s_%s", app, namespace, env, name);
+    }
+
+    public String toMetas() {
+        return JSON.toJSONString(this.getParameters());
     }
 }
