739b7d247f1de3470b25250799b74201845251f0
Win Wang <ewangtf@gmail.com>
2024-06-02 23:05:11 +0800

    add util methods for http invoker

--- winrpc-core/src/main/java/cn/winwang/winrpc/core/consumer/HttpInvoker.java
+++ winrpc-core/src/main/java/cn/winwang/winrpc/core/consumer/HttpInvoker.java
@@ -2,6 +2,12 @@ package cn.winwang.winrpc.core.consumer;
 
 import cn.winwang.winrpc.core.api.RpcRequest;
 import cn.winwang.winrpc.core.api.RpcResponse;
+import cn.winwang.winrpc.core.consumer.http.OkHttpInvoker;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import lombok.SneakyThrows;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Interface for http invoke.
@@ -11,6 +17,37 @@ import cn.winwang.winrpc.core.api.RpcResponse;
  */
 public interface HttpInvoker {
 
-    RpcResponse<?> post(RpcRequest rpcRequest, String url);
+    Logger log = LoggerFactory.getLogger(HttpInvoker.class);
+
+    HttpInvoker Default = new OkHttpInvoker(500);
+
+    RpcResponse post(RpcRequest rpcRequest, String url);
+
+    String post(String requestString, String url);
+    String get(String url);
+
+    @SneakyThrows
+    static <T> T httpGet(String url, Class<T> clazz) {
+        log.debug(" =====>>>>>> httpGet: " + url);
+        String respJson = Default.get(url);
+        log.debug(" =====>>>>>> response: " + respJson);
+        return JSON.parseObject(respJson, clazz);
+    }
+
+    @SneakyThrows
+    static <T> T httpGet(String url, TypeReference<T> typeReference) {
+        log.debug(" =====>>>>>> httpGet: " + url);
+        String respJson = Default.get(url);
+        log.debug(" =====>>>>>> response: " + respJson);
+        return JSON.parseObject(respJson, typeReference);
+    }
+
+    @SneakyThrows
+    static <T> T httpPost(String requestString,String url, Class<T> clazz) {
+        log.debug(" =====>>>>>> httpGet: " + url);
+        String respJson = Default.post(requestString, url);
+        log.debug(" =====>>>>>> response: " + respJson);
+        return JSON.parseObject(respJson, clazz);
+    }
 
 }
